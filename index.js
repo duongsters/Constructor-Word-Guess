67d8cef6787d97957bdd6f015ae40bfab9099f9c
Andrew Duong <duongsters@gmail.com>
2019-09-30 17:15:19 -0700

    started my inquirer DOM method that will ask for user to pick any letter from a-z

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var inquirer = require("inquirer");
-var randomWords = require("random-words");
+var randomWords = require("./list.js");
 var Word = require("./word.js")
 
+
+var guessing = [{
+    type:"input",
+    name:"userGuess",
+    message: "Type in any letter...from A to Z!",
+}]
